3d367d209c2c0f55a6a340814e9fa35199418a49
willsyn7 <synwill7777@gmail.com>
2025-07-31 07:43:24 -0700

    server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express  = require("express");
+const app = express();
+ const cors = require(`cors`);
+ const corsOptions = {
+    origin: ["http://localhost:5173"],
+
+
+ };
+ app.use(cors(corsOptions));
+
+
+app.get("/api",(req,res)=>{
+    res.json({fruits:["apple","orange","banana"]});
+})
+
+app.listen(8080,()=>{
+    console.log(`server started on port 8080`);
+});
